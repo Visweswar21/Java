3bde3a6877ebf4e6f0608fcc2aa8b8154cb9ac77
Visweswar21 <visweswarpamuru@gmail.com>
2025-06-02 14:46:51 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,24 @@
+package javaexamaple;
+import java.util.*;
+public class switchcase {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		switch(s) {
+		case "monday":
+			System.out.println("sreedher fucks sumanth");
+			break;
+		case "tuesday":
+			System.out.println("Lets us learn java");
+			break;	
+		case "wendesday":
+			System.out.println("who fuck sreedhar ");
+			break;
+		default:
+			System.out.println("no case is there");
+			break;
+		}
+	}
+}
+
+	
