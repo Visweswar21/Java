7c587e7ca8a99f8e1d9c32f89f1d90368de0c26e
Visweswar21 <visweswarpamuru@gmail.com>
2025-09-11 23:52:12 +0530

    Add files via upload

---
+++ invertedTriangle.java
@@ -0,0 +1,19 @@
+package javaexamaple;
+
+public class invertedTriangle {
+	public static void main(String[] args) {
+		int n=5;
+		pattern1(n);
+		
+	}
+	static void pattern1(int n) {
+		for(int row=0;row<=n;row++) {
+			for(int col=0;col<=n-row;col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
