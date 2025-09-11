64cb6f9aafd5fe32130234c52562abadbe48ebfe
Visweswar21 <visweswarpamuru@gmail.com>
2025-09-11 23:42:25 +0530

    Add files via upload

---
+++ rightTriangle.java
@@ -0,0 +1,22 @@
+package javaexamaple;
+
+public class rightTriangle {
+	
+		public static void main(String[] args) {
+			int n = 5;
+			pattern(n);
+		}
+		static void pattern(int n) {
+			for(int row=0;row<=n;row++) {
+				for(int col=0;col<=row;col++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+
+
+
